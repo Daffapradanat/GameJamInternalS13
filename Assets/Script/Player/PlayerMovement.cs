a582550f54a32455e8e3c908d4c06c27c6f4b34b
Daffa Pradana T. <neoleo1140@gmail.com>
2025-10-26 00:48:40 +0700

    Add Control Lie in player "Protype"

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -19,6 +19,14 @@ public class PlayerMovement : MonoBehaviour
 
     bool isMoving;
     int direction = 0;
+    
+    // ===== TAMBAHAN UNTUK CONTROL LIE =====
+    [HideInInspector]
+    public Vector2 inputModifier = Vector2.one; // Multiplier untuk input (untuk invert, dll)
+    [HideInInspector]
+    public bool useCustomInput = false; // Flag apakah pakai custom input
+    [HideInInspector]
+    public Vector2 customInput = Vector2.zero; // Custom input dari ControlLie
 
     private void Awake()
     {
@@ -35,20 +43,35 @@ public class PlayerMovement : MonoBehaviour
 
     void Move()
     {
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        float horizontal;
+        float vertical;
+        
+        // ===== CEK APAKAH ADA CUSTOM INPUT =====
+        if (useCustomInput)
         {
-            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
-            rb.velocity = new Vector2(rb.velocity.x, Input.GetAxis("Vertical") * speed);
+            horizontal = customInput.x;
+            vertical = customInput.y;
+        }
+        else
+        {
+            horizontal = Input.GetAxis("Horizontal") * inputModifier.x;
+            vertical = Input.GetAxis("Vertical") * inputModifier.y;
+        }
+        
+        if (horizontal != 0 || vertical != 0)
+        {
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+            rb.velocity = new Vector2(rb.velocity.x, vertical * speed);
             isMoving = true;
             
-            if(Input.GetAxis("Horizontal") > 0)
-                direction = 1; // kanan
-            else if (Input.GetAxis("Horizontal") < 0)
-                direction = 3; // kiri
-            else if (Input.GetAxis("Vertical") > 0)
-                direction = 0; // atas
-            else if (Input.GetAxis("Vertical") < 0)
-                direction = 2; // bawah
+            if(horizontal > 0)
+                direction = 1;
+            else if (horizontal < 0)
+                direction = 3;
+            else if (vertical > 0)
+                direction = 0;
+            else if (vertical < 0)
+                direction = 2;
         }
         else
         {
@@ -61,7 +84,6 @@ public class PlayerMovement : MonoBehaviour
     {
         if (isMoving)
         {
-            // Animation
             if (anim != null)
             {
                 anim.SetBool("isWalking", true);    
